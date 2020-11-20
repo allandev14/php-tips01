b91a03a3aa06bad175ac281f45ab54f755c6966a
allan sousa <allanportinfo@gmail.com>
2020-11-20 10:59:57 -0300

    projeto terminado

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -14,24 +14,24 @@ class InstalledVersions
 private static $installed = array (
   'root' => 
   array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
     'aliases' => 
     array (
     ),
-    'reference' => '32dadf270f251658743948a1e03ec32f8523c7f5',
+    'reference' => '087af59c93c28b1b7d40e99879e5234d1afe05b5',
     'name' => 'allandev14/phptips01',
   ),
   'versions' => 
   array (
     'allandev14/phptips01' => 
     array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
       'aliases' => 
       array (
       ),
-      'reference' => '32dadf270f251658743948a1e03ec32f8523c7f5',
+      'reference' => '087af59c93c28b1b7d40e99879e5234d1afe05b5',
     ),
     'phpmailer/phpmailer' => 
     array (
